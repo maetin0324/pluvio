3f94ab49e16294c6c3215a0c5c790f40ed1f7495
maetin0324 <maetin0324@gmail.com>
2025-06-05 15:24:07 +0900

    chore2

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use futures::stream::StreamExt;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
-static TOTAL_SIZE: usize = 16 * 1024 * 1024 * 1024;
+static TOTAL_SIZE: usize = 128 * 1024 * 1024 * 1024;
 static BUFFER_SIZE: usize = 1024 * 1024;
 
 fn main() {
