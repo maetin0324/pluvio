73b61e438b7dc0c18edaba4479f412ba7da3a812
maetin0324 <maetin0324@gmail.com>
2025-06-05 15:18:13 +0900

    test for pegasus

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn main() {
             .read(true)
             .write(true)
             .custom_flags(libc::O_DIRECT)
-            .open("/local/rmaeda/ucio_test.txt")
+            .open("/scr/ucio_test.txt")
             .expect("Failed to open file");
         let fd = file.as_raw_fd();
 
